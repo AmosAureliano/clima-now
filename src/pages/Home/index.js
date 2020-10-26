1d11d597c1e3f7dbc26a5369f6c9d7c1f4012e63
Amós Aureliano <sce.amos@gmail.com>
2020-10-26 11:29:05 -0300

    adicionando funcionalidades e finalizando responsividade

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,6 @@ import axios from 'axios';
 import './styles.css';
 
 import logo from '../../assets/imagens/logo.png';
-import minimum from '../../assets/imagens/minimum.png';
-import maximum from '../../assets/imagens/maximum.png';
 import loading from '../../assets/gifs/loading.gif';
 import nubladoNight from '../../assets/imagens/002-cloud-night.png';
 import nubladoMorning from '../../assets/imagens/004-clouds-sun.png';
@@ -15,15 +13,17 @@ import ceuLimpoNoite from '../../assets/imagens/013-half-moon.png';
 import nevoa from '../../assets/imagens/028-clouds.png';
 import trovoada from '../../assets/imagens/018-lighting.png';
 import neve from '../../assets/imagens/010-snow.png';
+import vento from '../../assets/imagens/024-wind.png';
+import umidade from '../../assets/imagens/003-rain.png';
 
 export default function Home(){
-    var data = new Date();
-    var dias = ['Domingo', 'Segunda Feira', 'Terça Feira', 'Quarta Feira', 'Quinta Feira', 'Sexta Feira', 'Sábado']
+    let data = new Date();
+    let dias = ['Domingo', 'Segunda Feira', 'Terça Feira', 'Quarta Feira', 'Quinta Feira', 'Sexta Feira', 'Sábado'];
     const [location, setLocation] = useState(false);
     const[weather, setWeather] = useState(false);
 
-    var imagem = '';
-
+    let imagem = '';
+    let velVento = 0;
     
     let getWeather = async (lat, long) => {
         let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
@@ -56,6 +56,7 @@ export default function Home(){
         )
     }else{
 
+        {/*Verificando nuvens*/}
 
         if(weather['weather']['0']['description'] == "céu limpo"){
             if(data.getHours() >= 5 && data.getHours() < 18){
@@ -84,9 +85,33 @@ export default function Home(){
             imagem = neve
         }
 
+        {/*Verificando velocidade do vento escala de beaufort para km*/}
 
-
-
+        if(weather['wind']['speed'] >=  1 && weather['wind']['speed'] < 2){
+            velVento = 4;
+        }else if(weather['wind']['speed'] >= 2 && weather['wind']['speed'] <3){
+            velVento =  8;
+        }else if(weather['wind']['speed'] >= 3 && weather['wind']['speed']  < 4){
+            velVento = 15;
+        }else if(weather['wind']['speed'] >= 4 && weather['wind']['speed'] < 5){
+            velVento =  25;
+        }else if(weather['wind']['speed']  >=5 && weather['wind']['speed'] < 6){
+            velVento = 34;
+        }else if(weather['wind']['speed']  >= 6 && weather['wind']['speed'] < 7){
+            velVento = 45;
+        }else if(weather['wind']['speed'] >= 7 && weather['wind']['speed'] < 8){
+            velVento = 58;
+        }else if(weather['wind']['speed'] >=8 && weather['wind']['speed'] < 9){
+            velVento = 69;
+        }else if(weather['wind']['speed'] >= 9 && weather['wind']['speed'] < 10){
+            velVento = 83;
+        }else if(weather['wind']['speed'] >= 10 && weather['wind']['speed'] < 11){
+            velVento = 96;
+        }else if(weather['wind']['speed'] >= 11 && weather['wind']['speed'] < 12){
+            velVento = 108;
+        }else if(weather['wind']['speed'] >= 12){
+            velVento = 150
+        }
         return(
             
             <div className="container">
@@ -110,6 +135,22 @@ export default function Home(){
                             
                         </div>       
                     </div>
+                    
+                    <div className="velVento">
+                        <img src={vento} alt="vento"/>
+                        <p>{velVento} Km/h</p>
+                    </div>
+
+                    <div className="umidade">
+                        <img src={umidade}/>
+                        <p>{weather['main']['humidity']}%</p>
+                    </div>
+
+                    
+                </div>
+
+                <div className="footer">
+                    <p>Projeto desenvolvido por Amós Aureliano</p>
                 </div>
             </div>
         )
